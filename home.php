0378b344dbf9c551d87a32e69900f4803cd2a2ad
sn0rp <98631572+sn0rp@users.noreply.github.com>
2024-10-21 22:08:52 -0500

    Auto commit

--- home.php
+++ home.php
@@ -84,6 +84,8 @@ $visitCount = incrementVisitCount();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta property="og:image" content="" />
+    <meta name="twitter:image" content="" />
     <title>&#9732;Snorp</title>
     <link rel="icon" href="/images/snorp-sprite.png" type="image/png">
     <link rel="stylesheet" href="/css/style.css">
